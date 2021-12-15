0063c7e3acb6a24bd8560a68e468e443e383fc69
root <root@softer000.cafe24.com>
2021-12-15 19:08:58 +0900

    first commit5

--- pages/item/comment.js
+++ pages/item/comment.js
@@ -2,7 +2,6 @@
 import BackLayout from '../../layout/BackLayout'
 import FeedCategory from '../../layout/FeedCategory'
 import Link from 'next/link'
-
 export default function Feed(){
     return (
         <BackLayout>
