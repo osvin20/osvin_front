62d20de47bb88c9efd0a0f25366415f945457358
root <root@softer000.cafe24.com>
2022-04-22 02:04:00 +0900

    20220422_yj

--- atomic/FooterInfo.js
+++ atomic/FooterInfo.js
@@ -15,33 +15,30 @@ export default function FooterInfo(){
       <div className={'footerinfo'}>
         <div className={"company_info"}>
           <div>
-            <p>사업자번호</p>
+            <p>대표자</p>
+            <span>{info.de_admin_company_owner} | {info.de_admin_company_name}</span>
+          </div>
+          <div>
+            <p>사업자등록번호</p>
             <span>{info.de_admin_company_saupja_no}</span>
-          </div>
-          <div>
-            <p>사업자명</p>
-            <span>{info.de_admin_company_name}</span>
-          </div>
-          <div>
-            <p>대표자명</p>
-            <span>{info.de_admin_company_owner}</span>
-          </div>
-          <div>
-            <p>주소(본사)</p>
-            <span>{info.de_admin_company_addr}</span>
-          </div>
-          <div>
-            <p>주소(이커머스 사업부)</p>
-            <span>경기도 고양시 킨텍스로 255 일산 디엠시티 스카이뷰 104동 1410호</span>
-          </div>
-          <div>
-            <p>고객센터</p>
-            <span>{info.de_admin_company_tel}</span>
           </div>
           <div>
             <p>통신판매업번호</p>
             <span>{info.de_admin_tongsin_no}</span>
           </div>
+          <div>
+            <p>주소</p>
+            <span>{info.de_admin_company_addr}</span>
+          </div>
+          {/* <div>
+            <p>주소(이커머스 사업부)</p>
+            <span>경기도 고양시 킨텍스로 255 일산 디엠시티 스카이뷰 104동 1410호</span>
+          </div> */}
+          <div>
+            <p>고객센터</p>
+            <span>{info.de_admin_company_tel}</span>
+          </div>
+
         </div>
       </div>
   )
